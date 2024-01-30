0f8c765dcc184c1c5e8058a66410e465bb7df485
Lietotajs <Lietotajs@K028_D07>
2024-01-30 09:58:42 +0200

    Add project files.

---
+++ Atkartojums_1_MGr_ITIA3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atkartojums_1_MGr_ITIA3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Random rand = new Random();
+            int skaitlis = rand.Next(1, 101);
+
+            Console.WriteLine(skaitlis);    
+
+        Start:
+
+            Console.WriteLine("Ievadi skaitli: ");
+            int skaitlis2 = int.Parse(Console.ReadLine());
+
+            if (skaitlis2 < skaitlis)
+            {
+                Console.WriteLine("Lielāks");
+                goto Start;
+            }
+            else if (skaitlis2 > skaitlis)
+            {
+                Console.WriteLine("Mazāks");
+                goto Start;
+            }
+            else 
+            {
+                Console.WriteLine("Skaitlis ir uzminēts!");  
+            }           
+        }
+    }
+}
